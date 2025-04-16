edbbe0dc3adaee2f301b1c2847994bea3b9ba951
eduardoblim <eduardobatistalima507@gmail.com>
2025-04-16 12:56:22 +0000

    adcionei comentarios.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,16 +16,18 @@ int main(){
    // Siga os comentários para implementar cada parte do desafio.
    //Teste larissa
    
-     // abaixo estao as variáveis separadas para cada atributo da carta 1.
+     // abaixo estao as variáveis e os nome para cada atributo da carta 1.
 
 
-       char estado[50];
-       char codigodacarta[50];
-       char cidade[50];
-       float populacao;
-       float areaemkm;
-       float pib;
-       int numerodepontosturisticos;
+       char estado1[50];
+       char codigodacarta1[50];
+       char cidade1[50];
+       float populacao1;
+       float areaemkm1;
+       float pib1;
+       int numerodepontosturisticos1;
+
+       // abaixo estao as variaveis e os para cada atributo da carta 2.
 
        char estado2[50];
        char codigodacarta2[50];
@@ -33,40 +35,47 @@ int main(){
        float populacao2;
        float area2;
        float pib2;
-       int numerodepontosturisticosdacidade2;
+       int numerodepontosturisticos2;
 
 
-;  
+;  // abaixo estao os printf para ser fornecidas as informacoes da carta 1.
+  // abaixo estao os scanf para ler os dados fornecidos pelo usuario para a carta 1. 
     
-     printf("digite o estado:\n");
-     scanf("%s", estado);
+     printf("digite o estado 1:\n");
+     scanf("%s", estado1);
 
-     printf("digite o codigo da carta:\n");
-     scanf("%s", codigodacarta);
+     printf("digite o codigo da carta 1:\n");
+     scanf("%s", codigodacarta1);
 
-     printf("digite a cidade:\n");
-     scanf("%s", cidade);
+     printf("digite a cidade 1:\n");
+     scanf("%s", cidade1);
 
-     printf("digite a populacao:\n");
-     scanf("%f", &populacao);
+     printf("digite a populacao 1:\n");
+     scanf("%f", &populacao1);
 
-     printf("digite a area:\n");
-     scanf("%f", &areaemkm);
+     printf("digite a area 1:\n");
+     scanf("%f", &areaemkm1);
 
-     printf("digite o pib:\n");
-     scanf("%f", &pib);
+     printf("digite o pib 1:\n");
+     scanf("%f", &pib1);
    
-     printf("digite pontos turisticos:\n");
-     scanf("%d", &numerodepontosturisticos);
+     printf("digite pontos turisticos 1:\n");
+     scanf("%d", &numerodepontosturisticos1);
+
+     // abaixo estao os printf da carta 1 para serem exibidas as informacoes da carta 1 na tela.
    
      printf("__________carta 1__________\n");
-     printf("estado: %s\n", estado);
-     printf("codigo da carta: %s\n", codigodacarta);
-     printf("cidade: %s\n", cidade);
-     printf("populacao: %f\n", populacao);
-     printf("area em km: %.4f\n",areaemkm);
-     printf("pib: %f\n",pib);
-     printf("pontos turisticos:%d\n",numerodepontosturisticos);
+     printf("estado 1: %s\n", estado1);
+     printf("codigo da carta 1: %s\n", codigodacarta1);
+     printf("cidade 1: %s\n", cidade1);
+     printf("populacao 1: %f\n", populacao1);
+     printf("area em km 1: %.4f\n",areaemkm1);
+     printf("pib 1: %f\n",pib1);
+     printf("pontos turisticos 1:%d\n",numerodepontosturisticos1);
+
+
+     // abaixo estao os printf para ser fornecidas as informacoes da carta 2.
+    //  abaixo estao os scanf para ler os dados fornecidos pelo usuario para carta 2 .
 
     printf("digite o estado 2:\n");
     scanf("%s", estado2);
@@ -87,8 +96,9 @@ int main(){
    scanf("%F", &pib2);
 
    printf("digite o numero de pontos turisticos da cidade 2:\n");
-   scanf("%d", &numerodepontosturisticosdacidade2);
+   scanf("%d", &numerodepontosturisticos2);
 
+   // abaixo estao os printf da carta 2 para serem exibidas as informacoes da carta 2 na tela.
    
    printf("__________carta 2__________\n");
    printf("estado 2: %s\n", estado2);
@@ -97,7 +107,7 @@ int main(){
    printf("populacao 2: %f\n", populacao2);
    printf("area 2:%.3f\n", area2);
    printf("pib 2: %f\n", pib2);
-   printf("numero de pontos turisticos da cidade 2: %d\n", numerodepontosturisticosdacidade2);
+   printf("numero de pontos turisticos da cidade 2: %d\n", numerodepontosturisticos2);
 
 
    return 0;
